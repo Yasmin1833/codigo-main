0132135f22c13f7eebee6df4131c3bca461b65a6
Yasmin Costa <yasmincostai833@gmail.com>
2025-08-11 16:32:35 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    char str [20] str 1 [20]
+    print F ("digite uma string");
+    gets (str);
+    print F ("digite outro striny:");
+    gets (str 1);
+    
+    if (SCMP (str,str 1| {
+        pirnt F ("sao iguais");
+    }
